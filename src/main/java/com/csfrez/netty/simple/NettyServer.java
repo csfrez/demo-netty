f7e6fb8d3697617d5308e8da41e0c582734304e4
Yangzhi <yangzhi@ddjf.com.cn>
2019-12-25 10:49:09 +0800

    新增示例

--- src/main/java/com/csfrez/netty/simple/NettyServer.java
+++ src/main/java/com/csfrez/netty/simple/NettyServer.java
@@ -2,58 +2,108 @@ package com.csfrez.netty.simple;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
-import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.string.StringDecoder;
+import io.netty.util.AttributeKey;
 import io.netty.util.concurrent.Future;
 import io.netty.util.concurrent.GenericFutureListener;
 
+/**
+ * 01: 服务端启动流程介绍[https://www.jianshu.com/p/ec3ebb396943]
+ * 要启动Netty服务端,必须要指定三类属性,分别是线程模型、IO模型、连接读写处理逻辑
+ * Netty服务端启动的流程是创建引导类给引导类指定线程模型,IO模型,连接读写处理逻辑,绑定端口之后服务端就启动起来
+ * bind方法是异步的通过异步机制来实现端口递增绑定
+ * Netty服务端启动额外的参数,主要包括给服务端channel或者channel设置属性值,设置底层TCP参数
+ */
 public class NettyServer {
 
-	public static void main(String[] args) {
-		ServerBootstrap serverBootstrap = new ServerBootstrap();
+    private static final int BEGIN_PORT = 8000;
+    private static final AttributeKey<Object> SERVER_NAME_KEY = AttributeKey.newInstance("serverName");
+    private static final String SERVER_NAME_VALUE = "nettyServer";
+    public static final AttributeKey<Object> CLIENT_KEY = AttributeKey.newInstance("clientKey");
+    public static final String CLIENT_VALUE = "clientValue";
 
-		NioEventLoopGroup boos = new NioEventLoopGroup();
-		NioEventLoopGroup worker = new NioEventLoopGroup();
-		serverBootstrap.group(boos, worker).channel(NioServerSocketChannel.class)
-				.option(ChannelOption.SO_BACKLOG, 1024)
-				.handler(new ChannelInitializer<NioServerSocketChannel>() {
-				    protected void initChannel(NioServerSocketChannel ch) {
-				        System.out.println("服务端启动中");
-				    }
-				})
-				.childOption(ChannelOption.SO_KEEPALIVE, true)
-				.childOption(ChannelOption.TCP_NODELAY, true)
-				.childHandler(new ChannelInitializer<NioSocketChannel>() {
-					@Override
-					protected void initChannel(NioSocketChannel ch) {
-						System.out.println("新连接处理");
-						ch.pipeline().addLast(new StringDecoder());
-						ch.pipeline().addLast(new SimpleChannelInboundHandler<String>() {
-							@Override
-							protected void channelRead0(ChannelHandlerContext ctx, String msg) {
-								System.out.println(Thread.currentThread().getName() + "==>" + msg);
-							}
-						});
-					}
-				});
-		bind(serverBootstrap, 8000);
-	}
+    /**
+     * 创建两个NioEventLoopGroup,这两个对象可以看做是传统IO编程模型的两大线程组,bossGroup表示监听端口,创建新连接的线程组,workerGroup表示处理每一条连接的数据读写的线程组
+     * 创建引导类 ServerBootstrap进行服务端的启动工作,通过.group(bossGroup, workerGroup)给引导类配置两大线程模型,指定服务端的IO模型为NIO,通过.channel(NioServerSocketChannel.class)来指定IO模型
+     * 调用childHandler()方法给引导类创建ChannelInitializer定义后续每条连接的数据读写,业务处理逻辑,泛型参数NioSocketChannel是Netty对NIO类型的连接的抽象,而NioServerSocketChannel也是对NIO类型的连接的抽象
+     * serverBootstrap.bind()是异步的方法调用之后是立即返回的,返回值是ChannelFuture,给ChannelFuture添加监听器GenericFutureListener,在GenericFutureListener的operationComplete方法里面监听端口是否绑定成功
+     * childHandler()用于指定处理新连接数据的读写处理逻辑,handler()用于指定在服务端启动过程中的一些逻辑
+     * attr()方法给服务端的channel即NioServerSocketChannel指定一些自定义属性,通过channel.attr()取出该属性,给NioServerSocketChannel维护一个map
+     * childAttr()方法给每一条连接指定自定义属性,通过channel.attr()取出该属性
+     * childOption()方法给每条连接设置一些TCP底层相关的属性:
+     * ChannelOption.SO_KEEPALIVE表示是否开启TCP底层心跳机制,true为开启
+     * ChannelOption.SO_REUSEADDR表示端口释放后立即就可以被再次使用,因为一般来说,一个端口释放后会等待两分钟之后才能再被使用
+     * ChannelOption.TCP_NODELAY表示是否开始Nagle算法,true表示关闭,false表示开启,通俗地说,如果要求高实时性,有数据发送时就马上发送,就关闭,如果需要减少发送次数减少网络交互就开启
+     * option()方法给服务端channel设置一些TCP底层相关的属性:
+     * ChannelOption.SO_BACKLOG表示系统用于临时存放已完成三次握手的请求的队列的最大长度,如果连接建立频繁,服务器处理创建新连接较慢,适当调大该参数
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
 
-	private static void bind(final ServerBootstrap serverBootstrap, final int port) {
-		serverBootstrap.bind(port).addListener(new GenericFutureListener<Future<? super Void>>() {
-			public void operationComplete(Future<? super Void> future) {
-				if (future.isSuccess()) {
-					System.out.println("端口[" + port + "]绑定成功!");
-				} else {
-					System.err.println("端口[" + port + "]绑定失败!");
-					bind(serverBootstrap, port + 1);
-				}
-			}
-		});
-	}
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        serverBootstrap.group(bossGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+//                .handler(new ChannelInitializer<ServerSocketChannel>() {
+//                	@Override
+//                	protected void initChannel(ServerSocketChannel ch) throws Exception {
+//                		System.out.println("服务端启动中");
+//                		System.out.println(ch.attr(SERVER_NAME_KEY).get());
+//                	}
+//                })
+                .handler(new ChannelInboundHandlerAdapter() {
+                    @Override
+                    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                        System.out.println("channelActive");
+                    }
+
+                    @Override
+                    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+                        System.out.println("channelRegistered");
+                    }
+
+                    @Override
+                    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+                        System.out.println("handlerAdded");
+                    }
+                })
+                .attr(SERVER_NAME_KEY, SERVER_NAME_VALUE)
+                .option(ChannelOption.SO_BACKLOG, 1024)
+                .childAttr(CLIENT_KEY, CLIENT_VALUE)
+                .childOption(ChannelOption.SO_KEEPALIVE, true)
+                .childOption(ChannelOption.SO_REUSEADDR, true)
+                .childOption(ChannelOption.TCP_NODELAY, true)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel ch) throws Exception {
+                        System.out.println(ch.attr(CLIENT_KEY).get());
+                        ch.pipeline().addLast(new StringDecoder());
+                        ch.pipeline().addLast(new MessageHandler());
+                    }
+                });
+
+        bind(serverBootstrap, BEGIN_PORT);
+    }
+
+    private static void bind(ServerBootstrap serverBootstrap, int port) {
+        serverBootstrap.bind(BEGIN_PORT).addListener(new GenericFutureListener<Future<? super Void>>() {
+            @Override
+            public void operationComplete(Future<? super Void> future) throws Exception {
+                if (future.isSuccess()) {
+                    System.out.println("端口[" + port + "]绑定成功!");
+                } else {
+                    System.err.println("端口[" + port + "]绑定失败!");
+                    bind(serverBootstrap, port + 1);
+                }
+            }
+        });
+    }
 }
